5eca3c58be038fe2ccaaae2a5fe80f38a4632c32
坂田宗章 <>
2022-12-11 07:02:11 +0900

    change icon and sidebar

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Main = () => {
   return (
     <main>
-      <div className="h-full ml-14 mt-14 mb-10 md:ml-64 w-3/4"></div>
+      <div className="h-full ml-14 md:ml-64 w-3/4 "></div>
     </main>
   );
 };
