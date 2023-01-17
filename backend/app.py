4577856acc8fac9a36bedf2b05f150483173cc43
RyutaroMatsumoto <m2002r1028@icloud.com>
2023-01-17 14:25:54 +0900

    [wip] flasとnextjsをつなげようとしている

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/api/profile', methods=["GET"])
 def index():
-    return 'Hello World'
+    return jsonify({"users": [
+        {"id": 1, "name": 'MuneyukiSakataaaaaaaaaaaaaa', "email": 'aaaa.com'},
+        {"id": 2, "name": 'Ryutaro00000000000000000000', "email": 'bbbbbbb.com'},
+        {"id": 3, "name": 'Nicoooooooooooooooooooooorn', "email": 'ccccccc.com'}
+    ]})
 
 
 if __name__ == "__main__":
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,35 +1,39 @@
+import axios from "axios"
+
 import Layout from "@/components/Layout";
 
 export default function Profile(props) {
   return (
     <Layout>
       {props.users.map((user) => {
-        return(
+        return (
           <p key={user.id}>
             <a>名前：{user.name}</a>
             <a>メールアドレス：{user.email}</a>
             <br></br>
           </p>
-        )
+        );
       })}
     </Layout>
   );
 }
 
 export async function getServerSideProps(context) {
+  console.log("1--------------------------------------------------------------------------------------------------------");
   try {
-    const host = context.req.headers.host || "localhost:5000";
-    const protocol = /^localhost/.test(host) ? "http" : "https";
-    const users = await fetch(`${protocol}://${host}/api/profile`).then((data) =>
-      data.json()
-    );
-    console.log({users})
+    const host = "127.0.0.1:8000";
+    const protocol = "http";
+    const url = `${protocol}://${host}/api/profile`;
+    const props = await axios.get(url);
+    const user = props.data;
+    console.log(user)
     return {
       props: {
-        users,
+        user,
       },
     };
-  } catch (e) {
+  }
+  catch (e) {
     console.log(e);
     return {
       props: {
