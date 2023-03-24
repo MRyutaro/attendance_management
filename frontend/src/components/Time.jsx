37021e949089c4bbb491778987ea682e9136af39
坂田宗章 <>
2023-03-25 08:18:56 +0900

    [fix]timebutton

--- frontend/src/components/Time.jsx
+++ frontend/src/components/Time.jsx
@@ -1,23 +1,59 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Time = () => {
-  const [date, setDate] = useState(new Date());
-  const time = date.toLocaleTimeString();
-  const day = date.toLocaleDateString();
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-    return () => clearInterval(timerId);
-  }, [date]);
+  const [clockInTime, setClockInTime] = useState(null);
+  const [clockOutTime, setClockOutTime] = useState(null);
+  const [clockInDate, setClockInDate] = useState(null);
+  const [clockOutDate, setClockOutDate] = useState(null);
+
+  let now = new Date();
+  let month = now?.getMonth() + 1;
+  let day = now?.getDate();
+  let hour = now?.getHours().toString().padStart(2, "0");
+  let minute = now?.getMinutes().toString().padStart(2, "0");
+  let dayOfWeek = now?.getDay();
+
+  const week = ["日", "月", "火", "水", "木", "金", "土"];
+  let clockDate = month + "月" + day + "日" + "[" + week[dayOfWeek] + "]";
+  let clockTime = hour + ":" + minute;
+
+  const handleInClick = () => {
+    setClockInTime(clockTime);
+    setClockInDate(clockDate);
+  };
+
+  const handleOutClick = () => {
+    setClockOutTime(clockTime);
+    setClockOutDate(clockDate);
+  };
 
   return (
-    <div className="clock flex">
-      <span>{day}</span>
-      <br></br>
-      <span className="ml-10" suppressHydrationWarning={true}>
-        {time}
-      </span>
+    <div>
+      <div className="flex flex-col md:flex-row justify-center items-center md:items-start md:justify-around  mt-20">
+        <button
+          onClick={handleInClick}
+          className="w-[200px] h-[200px] md:w-[230px] md:h-[230px] rounded-full border-2 border-black flex justify-center items-center text-3xl"
+        >
+          出勤
+        </button>
+        <p></p>
+        <button
+          onClick={handleOutClick}
+          className="w-[200px] h-[200px] md:w-[230px] md:h-[230px] rounded-full border-2 border-black flex justify-center items-center text-3xl md:mt-0 mt-10"
+        >
+          退勤
+        </button>
+      </div>
+      {clockInTime && (
+        <p>
+          出勤時刻：{clockInDate} {clockInTime}
+        </p>
+      )}
+      {clockOutTime && (
+        <p>
+          退勤時刻：{clockOutDate} {clockOutTime}
+        </p>
+      )}
     </div>
   );
 };
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -10,18 +10,6 @@ export default function Home() {
             <Time />
           </h1>
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center md:items-start md:justify-around  mt-20">
-          <button className="w-[200px] h-[200px] md:w-[230px] md:h-[230px] rounded-full border-2 border-black flex justify-center items-center text-3xl">
-            出勤
-          </button>
-          <button className="w-[200px] h-[200px] md:w-[230px] md:h-[230px] rounded-full border-2 border-black flex justify-center items-center text-3xl md:mt-0 mt-10">
-            退勤
-          </button>
-        </div>
-      </div>
-
-      <div>
-        aaa
       </div>
     </Layout>
   );
