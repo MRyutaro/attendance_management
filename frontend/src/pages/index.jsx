08dcb820be3bea0b8a9b077519a840df1ad7d26e
坂田宗章 <>
2023-03-29 07:13:07 +0900

    [add]Mantine

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,16 +1,13 @@
-import Layout from "@/components/Layout";
-import Time from "@/components/Time";
+import Time from "../components/Time";
 
 export default function Home() {
   return (
-    <Layout>
-      <div className="flex flex-col justify-center h-full">
-        <div className="text-center">
-          <h1 className="text-4xl">
-            <Time />
-          </h1>
-        </div>
+    <div className="flex flex-col justify-center h-full">
+      <div className="text-center">
+        <h1 className="text-4xl">
+          <Time />
+        </h1>
       </div>
-    </Layout>
+    </div>
   );
 }
