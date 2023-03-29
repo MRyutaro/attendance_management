a46b06332e367609745a9dd1bde4d7a92d35b62c
RyutaroMatsumoto <m2002r1028@icloud.com>
2023-03-29 21:29:05 +0900

    fix: flaskからfastapiに変更した

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,31 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI
+import uvicorn
 
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route('/api/profile', methods=["GET"])
-def index():
-    return jsonify({"users": [
-        {"id": 1, "name": 'MuneyukiSakataaaaaaaaaaaaaa', "email": 'aaaa.com'},
-        {"id": 2, "name": 'Ryutaro00000000000000000000', "email": 'bbbbbbb.com'},
-        {"id": 3, "name": 'Nicoooooooooooooooooooooorn', "email": 'ccccccc.com'}
-    ]})
+@app.get("/")
+def read_root():
+    return {"This is": "root"}
+
+
+@app.get("/api")
+def read_api():
+    return {"This is": "api"}
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    uvicorn.run(app, port=8000)
+
+# from flask import Flask, jsonify
+
+# app = Flask(__name__)
+
+
+# @app.route('/api/profile', methods=["GET"])
+# def index():
+#     return jsonify({"users": [
+#         {"id": 1, "name": 'MuneyukiSakataaaaaaaaaaaaaa', "email": 'aaaa.com'},
+#         {"id": 2, "name": 'Ryutaro00000000000000000000', "email": 'bbbbbbb.com'},
+#         {"id": 3, "name": 'Nicoooooooooooooooooooooorn', "email": 'ccccccc.com'}
+#     ]})
