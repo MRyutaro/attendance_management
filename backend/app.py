b3431c311e280808088a348da6d7dfbc9359b280
RyutaroMatsumoto <m2002r1028@icloud.com>
2023-03-29 22:55:17 +0900

    add: getメソッドを大方書いた

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
+import json
+import os
 
 app = FastAPI()
 
@@ -14,18 +16,55 @@ def read_api():
     return {"This is": "api"}
 
 
+@app.get("/api/companies")
+def read_companies():
+    # dataディレクトリの中のフォルダ名を取得する
+    companies = [
+        int(d) for d in os.listdir("../data")
+        if os.path.isdir(os.path.join("../data", d))
+    ]
+    return {"companies": companies}
+
+
+@app.get("/api/companies/{company_id}")
+def read_company(company_id: int):
+    # data/001/info.jsonからデータを取得する
+    with open(f"../data/{company_id}/info.json", "r") as f:
+        company_info = json.load(f)
+    return {
+        "company_id": company_id,
+        "company_info": company_info
+    }
+
+
+@app.get("/api/companies/{company_id}/employees")
+def read_employees(company_id: int):
+    with open(f"../data/{company_id}/info.json", "r") as f:
+        employees = json.load(f)["employees"]
+    return {
+        "company_id": company_id,
+        "employees": employees
+    }
+
+
+@app.get("/api/companies/{company_id}/employees/{employee_id}")
+def read_employee(company_id: int, employee_id: int):
+    with open(f"../data/{company_id}/{employee_id}/info.json", "r") as f:
+        employees = json.load(f)
+    # ../data/1/1/monthly_data/の中のファイル名の拡張子を除いたものを取得する
+    monthly_data = [
+        int(d.split(".")[0]) for d in os.listdir(f"../data/{company_id}/{employee_id}/monthly_data")
+        if os.path.isfile(os.path.join(f"../data/{company_id}/{employee_id}/monthly_data", d))
+    ]
+
+    return {
+        "company_id": company_id,
+        "employee_id": employee_id,
+        "employee_neme": employees["name"],
+        "authority": employees["authority"],
+        "monthly_data": monthly_data
+    }
+
+
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
-
-# from flask import Flask, jsonify
-
-# app = Flask(__name__)
-
-
-# @app.route('/api/profile', methods=["GET"])
-# def index():
-#     return jsonify({"users": [
-#         {"id": 1, "name": 'MuneyukiSakataaaaaaaaaaaaaa', "email": 'aaaa.com'},
-#         {"id": 2, "name": 'Ryutaro00000000000000000000', "email": 'bbbbbbb.com'},
-#         {"id": 3, "name": 'Nicoooooooooooooooooooooorn', "email": 'ccccccc.com'}
-#     ]})
