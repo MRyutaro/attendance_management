d5ee4c7c396cee56cb0639020fc775f1571c6a87
nico <hirahiracorocoro@gmail.com>
2023-04-02 01:55:30 +0900

    [fix]パスワード変更画面と登録画面に変更

---
+++ frontend/src/pages/Forgotpass.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Forgotpass = () => {
+  return (
+    <div className="Forgotpass">
+      <h2>Login</h2>
+      <h3>パスワード変更画面</h3>
+    </div>
+  );
+};
+
+export default Forgotpass;
---
+++ frontend/src/pages/Singup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Singup = () => {
+  return (
+    <div className="Singups">
+      <h2>Singup</h2>
+      <h3>登録画面</h3>
+    </div>
+  );
+};
+
+export default Singup;
