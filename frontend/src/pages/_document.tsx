0e1df9d2d57dc9345130c0eef75922f266d69002
坂田宗章 <>
2023-04-04 04:54:15 +0900

    [add] 申請

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,7 +1,26 @@
 import { createGetInitialProps } from "@mantine/next";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+import { siteMeta } from "../lib/constants";
+
 const getInitialProps = createGetInitialProps();
+
+const { siteIcon } = siteMeta;
+interface SiteMeta {
+  siteTitle: string;
+  siteIcon: string;
+}
+
+interface MetaProps {
+  pageTitle?: string;
+  siteMeta: SiteMeta;
+}
+
+const Meta: React.FC<MetaProps> = ({ pageTitle, siteMeta }) => {
+  const { siteTitle } = siteMeta;
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
+  return <title>{title}</title>;
+};
 
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
@@ -9,7 +28,11 @@ export default class _Document extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <Meta siteMeta={siteMeta} />
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          <link rel="icon" href={siteIcon} />
+        </Head>
         <body>
           <Main />
           <NextScript />
