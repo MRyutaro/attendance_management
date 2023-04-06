eeb71072dcce088b9abfee14d0966ea21ba9f26a
nico <hirahiracorocoro@gmail.com>
2023-04-06 23:06:19 +0900

    [add]個人設定準備2

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import { useRouter } from "next/router";
 
-function Profile() {
-  return <div>Profile</div>;
-}
+const Profile = () => {
+  const router = useRouter();
+  const clickHandler = () => {
+    router.push("/", "home");
+  };
+  return (
+    <>
+      {}
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">個人設定</h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="namesetting" className="sr-only">
+                namesetting
+                </label>
+                <input
+                  id="namesetting"
+                  name="namesetting"
+                  type="pnamesetting"
+                  autoComplete="new-namesetting"
+                  required
+                  className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Name"
+                />
+              </div>
+              <br></br>
+              <div>
+                <label htmlFor="staffemail" className="sr-only">
+                staffemail
+                </label>
+                <input
+                  id="staffemail"
+                  name="staffemail"
+                  type="pstaffemail"
+                  autoComplete="new-staffemail"
+                  required
+                  className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                onClick={clickHandler}
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <div className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                </span>
+                Submit
+              </button>
+            </div>
+            <div className="justify-between">
+              <div className="text-sm"></div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
