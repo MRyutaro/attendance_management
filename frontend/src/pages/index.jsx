b1d2e594ae9c363d8d115a01f712da120339e089
坂田宗章 <>
2023-04-12 21:35:37 +0900

    [add]避難

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,5 +1,23 @@
+import axios from "axios";
+
 import Clock from "../components/Clock";
 import Calender from "../components/Clock/Calender";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8000/api/v1", // APIエンドポイントが存在するサーバーのURL
+});
+const fetchCompanyData = async () => {
+  const response = await instance.get(`/companies/1`);
+  const data = response.data;
+  console.log(data);
+  return data;
+};
+const CompanyData = async () => {
+  const response = await instance.get(`/companies/1`);
+  const data = response.data;
+  console.log(data);
+  return data;
+};
 
 export default function Home() {
   return (
@@ -7,6 +25,7 @@ export default function Home() {
       <div className="text-center">
         <Clock />
         <Calender />
+        <button onClick={CompanyData}>click</button>
       </div>
     </div>
   );
