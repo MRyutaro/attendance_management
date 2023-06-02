c013a37cfc66fd63d3c6708a293d93cd725cef4c
nico <hirahiracorocoro@gmail.com>
2023-06-03 01:02:48 +0900

    [fix]ログイン画面を日本語表記に変更

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -44,7 +44,7 @@ const Login = () => {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">ログイン</h2>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Wakreate勤怠</h2>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
@@ -60,7 +60,7 @@ const Login = () => {
                   autoComplete="current-email"
                   required
                   className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Email or Username"
+                  placeholder="メールアドレス"
                   value={employee_email}
                   onChange={(event) => setUseremail(event.target.value)}
                 />
@@ -77,7 +77,7 @@ const Login = () => {
                   autoComplete="current-password"
                   required
                   className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Password"
+                  placeholder="パスワード"
                   value={employee_login_password}
                   onChange={(event) => setPassword(event.target.value)}
                 />
@@ -87,7 +87,7 @@ const Login = () => {
             <div className="flex items-center justify-between">
               <div className="text-sm">
                 <Link href="/Forgotpass" className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Forgot your password?
+                  パスワードを再発行する
                 </Link>
               </div>
             </div>
@@ -101,14 +101,14 @@ const Login = () => {
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <div className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
                 </span>
-                Login
+                ログイン
               </button>
             </div>
             <div className="justify-between">
               <div className="text-sm">
-                <p>Do not have an account?</p>
+                <p>※初めての方は新規登録を行ってください</p>
                 <a href="Singup" className="flex justify-end font-medium text-indigo-600 hover:text-indigo-500">
-                  Sing Up
+                  新規登録はこちら
                 </a>
               </div>
             </div>
